21422504161c095f44f43370a9cd76df077a040c
khalil <elmaghraoui.khalil.1@gmail.com>
2020-05-19 05:05:04 +0000

    Create orders.php

---
+++ admin/orders.php
@@ -0,0 +1,14 @@
+<?php 
+			session_start();
+			require_once '../config/connecte.php'   ;  
+			if(!isset($_SESSION['email']) &  empty($_SESSION['email']) )
+			{
+				header('location: login.php');
+				exit();
+			}
+				 
+				 
+			
+
+
+      ?>
