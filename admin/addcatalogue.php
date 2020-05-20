ebc91db132b2154ed5f691d117272f575abfa7ec
khalil <elmaghraoui.khalil.1@gmail.com>
2020-05-20 22:59:10 +0000

    enter

--- admin/addcatalogue.php
+++ admin/addcatalogue.php
@@ -1,6 +1,6 @@
 <?php 
 			session_start();
-			// require_once '../config/connecte.php'   ;  
+			require '../config/connecte.php'   ;  
 	       if(!isset($_SESSION['email']) &  empty($_SESSION['email']) )
 			{
 				header('location: login.php');
