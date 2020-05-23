2cd7234b8cc071293097a7dd1928fb90984f620a
khalil <elmaghraoui.khalil.1@gmail.com>
2020-05-23 01:28:27 +0000

    enter

--- admin/editcategory.php
+++ admin/editcategory.php
@@ -13,12 +13,12 @@
 
             }else
             {
-                header('location : catelogue.php');
+                header('location : catalogue.php');
             }
 
 		 if(isset($_POST) & !empty($_POST))
 		 {
-             
+              
 
 			 $id = mysqli_real_escape_string($connection, $_POST['id']);
 			 $name = mysqli_real_escape_string($connection, $_POST['categoryname']);
@@ -47,14 +47,15 @@
 			<div class="container">
 			<form method="post">
 			 <div class="form-group">
-                 <label for="productname">category name</label>
-                 <?php 
+                 <label for="categoryname">category name</label>
+				 <?php 
+				 require_once '../config/connecte.php'   ;  
   $sql="SELECT * FROM category WHERE id = $id ";
   $res = mysqli_query($connection,$sql);
   $r = mysqli_fetch_assoc($res) ;
 
       ?>
-                <input type="button" type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+                <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
 				 <input type="text"  class="form-control" name="categoryname"  id="categoryname" placeholder="category name" value="<?php echo $r['name']; ?>">
 			 </div>
 			<button type="submit" class="btn btn-default">submit</button>
